42889ebc5f8e4e578eb9a3d93572be0bdf34762c
Maxim Koshkin <humblerat@mail.ru>
2025-07-10 22:09:53 +0700

    updated user table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('username')->nullable()->unique();
+            $table->string('password')->nullable();
+            $table->string('email')->nullable()->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('avatar_url')->nullable();
+            $table->date('birthday')->nullable();
+            $table->date('career_start')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
