d7d1c800414a46332744f0bd714ce489bcaf9a70
Maxim Koshkin <humblerat@mail.ru>
2025-08-04 20:32:50 +0700

    added new actions (any-like)

--- app/Enums/Action.php
+++ app/Enums/Action.php
@@ -7,15 +7,18 @@ enum Action
     case Create;
     case View;
     case ViewOwn;
+    case ViewAny;
     case ViewPremium;
     case ViewDeleted;
     case Edit;
     case EditOwn;
     case Delete;
     case DeleteOwn;
+    case DeleteAny;
     case Approve;
     case Reject;
     case Ban;
+    case BanAny;
     case Unban;
     case Favorite;
     case Vote;
