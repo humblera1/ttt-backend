06d1bec5223f89f93e1c9597c440f5c91aa3a70f
Maxim Koshkin <humblerat@mail.ru>
2025-10-02 16:35:31 +0700

    added pagination logic when searching on the /questions/list endpoint

--- app/Services/api/v1/QuestionService.php
+++ app/Services/api/v1/QuestionService.php
@@ -4,12 +4,12 @@ namespace App\Services\api\v1;
 
 use App\DTOs\v1\Question\QuestionsFilterDTO;
 use App\Http\Filters\v1\Question\QuestionsListFilter;
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 class QuestionService
 {
-    public function getQuestionsList(QuestionsFilterDTO $dto): Collection
+    public function getQuestionsList(QuestionsFilterDTO $dto): LengthAwarePaginator
     {
-        return new QuestionsListFilter($dto)->apply()->get();
+        return new QuestionsListFilter($dto)->apply()->paginate(setting('question.per_page', 15));
     }
 }
