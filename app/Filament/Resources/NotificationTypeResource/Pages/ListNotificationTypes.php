a41a66f50b290d0746e2f09b224fa93ccc622765
Максим Юшков <m.yushkov@napoleonit.ru>
2026-01-06 15:18:35 +0700

    Phase-VI: Завершена разработка ресурса для типа уведомления

--- app/Filament/Resources/NotificationTypeResource/Pages/ListNotificationTypes.php
+++ app/Filament/Resources/NotificationTypeResource/Pages/ListNotificationTypes.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\NotificationTypeResource\Pages;
 
 use App\Filament\Resources\NotificationTypeResource;
-use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListNotificationTypes extends ListRecords
