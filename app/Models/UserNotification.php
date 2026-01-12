901bfd7037ff84331e61c0f98ee11e24dd7d97e3
Максим Юшков <m.yushkov@napoleonit.ru>
2026-01-12 13:39:51 +0700

    Phase-VII: Добавлено правило каста в модель in-app уведомлений

--- app/Models/UserNotification.php
+++ app/Models/UserNotification.php
@@ -6,11 +6,12 @@ use App\Policies\UserNotificationPolicy;
 use Illuminate\Database\Eloquent\Attributes\UsePolicy;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 #[UsePolicy(UserNotificationPolicy::class)]
 class UserNotification extends Model
 {
-    //
+    use SoftDeletes;
 
     public function type(): BelongsTo
     {
@@ -20,5 +21,17 @@ class UserNotification extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'data' => 'array',
+        ];
     }
 }
