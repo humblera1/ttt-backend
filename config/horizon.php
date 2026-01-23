ca4c10c098b5d560dceb5005518333a8241bd413
Максим Юшков <m.yushkov@napoleonit.ru>
2026-01-23 14:41:53 +0700

    Phase-VII: Обновлена конфигурация horizon - добавлена очередь notification

--- config/horizon.php
+++ config/horizon.php
@@ -213,11 +213,30 @@ return [
             'timeout' => 60,
             'nice' => 0,
         ],
+
+        'notification-supervisor' => [
+            'connection' => 'redis',
+            'queue' => [
+                Queue::Notifications->value,
+            ],
+            'balance' => 'simple',
+            'autoScalingStrategy' => 'time',
+            'maxProcesses' => 1,
+            'maxTime' => 0,
+            'maxJobs' => 0,
+            'memory' => 128,
+            'tries' => 1,
+            'timeout' => 60,
+            'nice' => 0,
+        ],
     ],
 
     'environments' => [
         'local' => [
             'suggestions-supervisor' => [
+                'maxProcesses' => 1,
+            ],
+            'notification-supervisor' => [
                 'maxProcesses' => 1,
             ],
         ],
